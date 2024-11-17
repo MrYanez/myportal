bd8712639bcb8aaf0d4dbaca396c26c71fd4817a
MrYanez <129912196+MrYanez@users.noreply.github.com>
2024-11-16 16:10:33 -0800

    Add files via upload

---
+++ myportal/contact.php
@@ -0,0 +1,24 @@
+
+<?php include 'portal-config.php';?>
+<?php include 'includes/header.php';?>
+<h2 class="subheader"><?=$PageID?>Contact.php</h2>
+        <?php
+        /*
+         * Below are 2 different forms to be re-used       
+         * 
+         * Only use one at a time, comment out the other!       
+         *
+         */
+
+        include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "yaneznahum123@gmail.com";  //place your/your client's email address here
+        $toName = "Nahum"; //place your client's name here
+        $website = "IT162 Contact form";  //place NAME of your client's website
+
+        //echo loadContact('simple.php');#demonstrates a simple contact form
+        echo loadContact('multiple.php');#demonstrates multiple form elements
+
+	?>
+
+<?php include 'includes/footer.php';?>
---
+++ myportal/includes/header.php
@@ -0,0 +1,31 @@
+
+<!--This is myportal header.html-->
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Nahum Yanez</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <script src="https://use.fontawesome.com/6a71565c22.js"></script>
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/nav.css">
+  <link rel="stylesheet" href="css/forms.css">
+ </head>
+ <body>
+     <header>
+     <h1>Nahum Yanez</h1>
+        <nav class="topnav" id="myTopnav">
+            <a href="https://nahyan.dreamhosters.com/myportal/index.php" class="_blank">Home</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+        </nav>
+     </header>
+     
+   <main class="wrapper">
+    <!--header ends here, applied by nahum-->
+    
+    <!--<picture class="profile">
+            <source media="(min-width: 1200px)" srcset="images/img_desktop.png">
+            <source media="(min-width: 501px)" srcset="images/img_desktop.png">
+            <img src="images/img_desktop.png" alt="">
+        </picture>  -->
+        <h2 class="subheader"></h2> 
